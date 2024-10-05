03a4bb8f2abf963a9a7e1112f79837b58ee04380
ira <thesaramark1992@gmail.com>
2024-10-05 09:42:15 +0600

    Update firstBanner.tsx

--- shared/components/firstBanner.tsx
+++ shared/components/firstBanner.tsx
@@ -9,7 +9,7 @@ export const FirstBanner = () => {
                 <p>[02] - Прайс-лист</p>
             </div>
             <h1>Скачайте наш <br /> Прайс-лист в PDF</h1>
-            <a href="https://drive.google.com/file/d/1G9JtMvq76Ynxev6tQAR9Fz4uYRJL-8wx/view?usp=drive_link">
+            <a href="https://drive.google.com/file/PDF_FILE">
                 <div className="pdf-btn">
                     <p>Скачать в PDF</p>
                     <div className="pdf-icon">
