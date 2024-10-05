88b81bbf4880e86928cc3efa3cc0456e85efd995
ira <thesaramark1992@gmail.com>
2024-10-05 09:45:23 +0600

    Update secondBanner.tsx

--- shared/components/secondBanner.tsx
+++ shared/components/secondBanner.tsx
@@ -11,14 +11,14 @@ export const SecondBanner = () => {
               <h1>Хотите узнать больше? <br /> Напишите нам на WhatsApp или позвоните</h1>
             </div>
             <div className="s-banner__btns">
-            <a href="https://wa.me/77772786098" target='_blank' className='whataspp-btn'>
+             <a href="https://wa.me/UR_NUMBER" target='_blank' className='whataspp-btn'>
                 <div className="text-s btn">
                   <p>Написать нам</p>
                   <img src={arrpwbtn} alt="arrpwbtn" />
                 </div>  
               </a>
 
-              <a href="tel:+77011829505"  target='_blank'>
+              <a href="tel:+UR_NUMBER"  target='_blank'>
                 <div className="call-s btn">
                   <p>Позвонить нам</p>
                   <div className="callbtn">
